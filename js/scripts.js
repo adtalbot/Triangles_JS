a28b7c1a02d8032da8740f268dacd6a8a31c0778
audrie <audrie.talbot@gmail.com>
2015-07-08 18:54:00 -0700

    Fully functioning.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
 var triangleFinder = function(side_1, side_2, side_3) {
   if ((side_1 === side_2) && (side_2 === side_3)) {
-    return 'This triangle is an equilateral triangle.';
+    return 'equilateral';
 
   } else if ((side_1 === side_2) || (side_1 === side_3) || (side_2 === side_3)) {
-    return 'This triangle is an isosceles triangle.';
+    return 'isosceles';
 
   } else if ((side_1 >= (side_2 + side_3)) || (side_2 >= (side_1 + side_3)) || (side_3 >= (side_1 + side_2))) {
-    return 'This is not a triangle.';
+    return 'not a valid';
 
   } else  {
-    return 'This triangle is a scalene triangle.';
+    return 'scalene';
   };
 };
+
+
+$(document).ready(function() {
+  $('form#triangle-finder').submit(function(event) {
+    var side_one = parseInt($('input#side_one').val());
+    var side_two = parseInt($('input#side_two').val());
+    var side_three = parseInt($('input#side_three').val());
+
+    var result = triangleFinder(side_one, side_two, side_three);
+
+    $('.side_one').text(side_one);
+    $('.side_two').text(side_two);
+    $('.side_three').text(side_three);
+    $('.result').text(result);
+
+    $('#result').show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe('triangleFinder', function() {
   it('will return equilateral if all sides are equal', function() {
-    expect(triangleFinder(2,2,2)).to.equal('This triangle is an equilateral triangle.');
+    expect(triangleFinder(2,2,2)).to.equal('equilateral');
   });
 
   it('will return isosceles if two sides are equal', function() {
-    expect(triangleFinder(2,2,5)).to.equal('This triangle is an isosceles triangle.');
+    expect(triangleFinder(2,2,5)).to.equal('isosceles');
   });
 
   it('will return scalene if no sides are equal', function() {
-    expect(triangleFinder(2,3,4)).to.equal('This triangle is a scalene triangle.');
+    expect(triangleFinder(2,3,4)).to.equal('scalene');
   });
 
   it('will return not a triangle if one side is the same or larger than the sum of the other two sides', function() {
-    expect(triangleFinder(2,3,7)).to.equal('This is not a triangle.');
+    expect(triangleFinder(2,3,7)).to.equal('not a valid');
   });
 });
