93c3b812c2ef52d01fee0a9261acb432dc783204
audrie <audrie.talbot@gmail.com>
2015-07-08 17:47:24 -0700

    Passes first three specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 var triangleFinder = function(side_1, side_2, side_3) {
-  return 'equilateral';
+  if ((side_1 === side_2) && (side_2 === side_3)) {
+    return 'This triangle is an equilateral triangle.';
+  } else if ((side_1 === side_2) || (side_1 === side_3) || (side_2 === side_3)) {
+    return 'This triangle is an isosceles triangle.';
+  } else {
+    return 'This triangle is a scalene triangle.';
+  };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe('triangleFinder', function() {
   it('will return equilateral if all sides are equal', function() {
-    expect(triangleFinder(2,2,2)).to.equal('equilateral');
+    expect(triangleFinder(2,2,2)).to.equal('This triangle is an equilateral triangle.');
+  });
+
+  it('will return isosceles if two sides are equal', function() {
+    expect(triangleFinder(2,2,5)).to.equal('This triangle is an isosceles triangle.');
+  });
+
+  it('will return scalene if no sides are equal', function() {
+    expect(triangleFinder(2,3,4)).to.equal('This triangle is a scalene triangle.');
   });
 });
